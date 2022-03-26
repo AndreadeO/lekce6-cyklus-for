4a4b84936a3201b31a024055c611e718803069c4
Andrea de Oliveira <soudkova.a@gmail.com>
2022-03-27 00:48:13 +0100

    for loop from previous lecture added to main branch

--- main.js
+++ main.js
@@ -7,7 +7,12 @@ console.log('První příklad - vzestupná řada 0 - 10');
 
 //TADY NAPIS CYKLUS
 
-console.log('-------------------');
+for (let i = 0; i <= 10; i++) {
+
+    console.log(i);
+    
+}
+
 
 
 
@@ -18,14 +23,36 @@ console.log('-------------------');
 console.log('Druhý příklad - rozdíl mezi <= a <');
 
 //TADY NAPIS CYKLUS
+for (let i = 0; i < 10; i++) {
 
-console.log('-------------------');
-
+    console.log(i);
+    
+}
+    
 
 
 // Třetí příklad - vypiš sestupně čísla od 10 do 0
 console.log('Třetí příklad - sestupná řada 10 - 0');
 
 //TADY NAPIS CYKLUS
+for (let i = 10; i >= 0; i--) {
 
-console.log('-------------------');
+    console.log(i);
+    
+}
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
